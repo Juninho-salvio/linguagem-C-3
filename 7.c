d2f57106537d058156e05728e7f6ccb5fabdaa51
Sálvio De Oliveira Santos Júnior <79759829+Juninho-salvio@users.noreply.github.com>
2021-09-10 15:41:56 -0300

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+int i;
+char nome[100];
+float salario, aliquota;
+
+for (i=1; i<=10; i++) {
+
+printf("Digite o nome: \n");
+fflush(stdin);
+fgets(nome, 100, stdin); //ler uma string num arquivo
+
+printf("Digite o salario: \n");
+scanf("%f", &salario);
+
+if (salario <= 1300) {
+printf("Isento de imposto \n");
+}
+else {
+if (salario <= 2300) {
+aliquota = (salario * 10) / 100;
+}
+else {
+aliquota = (salario * 15) / 100;
+}
+printf("Aliquota: %f \n", aliquota);
+}
+}
+}
